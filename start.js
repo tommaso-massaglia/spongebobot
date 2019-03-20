bda528d68946f10985f36cf083948e1896cbad92
Tommaso Massaglia <48190278+tommaso-massaglia@users.noreply.github.com>
2019-03-20 01:02:26 +0100

    initial commit

---
+++ start.js
@@ -0,0 +1,25 @@
+const TeleBot = require ('telebot');
+const isOdd = require ('is-odd');
+
+const bot = new TeleBot('476613891:AAGRsfZ6CdXiKTu74R2kBIbKvqevnXwIHA0');
+
+bot.on('text', (msg) => {
+    let messaggio = msg.text;
+    return msg.reply.text(modificatesto(messaggio));
+});
+
+function modificatesto(messaggio){
+    let testo = "";
+    let inserimento = messaggio;
+    for (let i=0; i<inserimento.length; i++){
+        if (!isOdd(i)){
+            testo+=messaggio[i].toUpperCase();
+        }
+        else {
+            testo+=messaggio[i].toLowerCase();
+        }
+}
+    return testo;
+}
+
+bot.start();
