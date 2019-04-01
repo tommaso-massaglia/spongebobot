6b0d7eea1cf8d9b3be7e63c2d689fa49d5b85a55
Tommaso Massaglia <tmassagl@gmail.com>
2019-04-01 09:51:59 +0200

    Improved the modificatesto function

--- start.js
+++ start.js
@@ -11,14 +11,32 @@ bot.on("text", msg => {
 
 function modificatesto(messaggio) {
   let testo = "";
-  let inserimento = messaggio;
+  let inserimento = messaggio.toLowerCase();
+
+  let random = (Math.random() * 10).toFixed(0);
+
+  if (random % 2) {
+    inserimento = inserimento.replace(/a/g, "Æ");
+    inserimento = inserimento.replace(/e/g, "Ē");
+    inserimento = inserimento.replace(/i/g, "ì");
+    inserimento = inserimento.replace(/o/g, "ő");
+    inserimento = inserimento.replace(/u/g, "ù");
+  } else {
+    inserimento = inserimento.replace(/a/g, "ą");
+    inserimento = inserimento.replace(/e/g, "é");
+    inserimento = inserimento.replace(/i/g, "Ĩ");
+    inserimento = inserimento.replace(/o/g, "Œ");
+    inserimento = inserimento.replace(/u/g, "Ü");
+  }
+
   for (let i = 0; i < inserimento.length; i++) {
-    if (!isOdd(i)) {
-      testo += messaggio[i].toUpperCase();
+    if (i % 2) {
+      testo += inserimento[i].toUpperCase();
     } else {
-      testo += messaggio[i].toLowerCase();
+      testo += inserimento[i].toLowerCase();
     }
   }
+
   return testo;
 }
 
